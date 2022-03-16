6228704f92640a29481ab14d2973bd57bda7d564
gneshan <60718950+gneshan@users.noreply.github.com>
2022-03-15 17:58:08 -0700

    Added log4j and config properties file

--- apitest/src/main/java/apitestResReq/BaseClass.java
+++ apitest/src/main/java/apitestResReq/BaseClass.java
@@ -1,15 +1,40 @@
 package apitestResReq;
 
-import org.testng.annotations.BeforeClass;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.testng.annotations.BeforeTest;
 
 import io.restassured.RestAssured;
 
 public class BaseClass {
 	
-	@BeforeClass
-	public void setup() {
+	Logger logger=Logger.getLogger("Test");
+	
+
+	Properties properties = new Properties();
 		
-		RestAssured.baseURI="https://reqres.in/api";
+	@BeforeTest
+	public void setup() throws IOException {
+		
+		FileInputStream fileinput;
+		try {
+			fileinput = new FileInputStream("config.properties");
+			properties.load(fileinput);
+			String baseuri = properties.getProperty("baseURL");
+			PropertyConfigurator.configure("./log4j.properties");
+			logger.setLevel(Level.INFO);
+			RestAssured.baseURI=baseuri;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
 	}
 
 }
